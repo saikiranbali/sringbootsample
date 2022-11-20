be9b4890aa6dbcb047a7061e187a55b58bb712a5
Sai Kiran <109275107+Saikirangg@users.noreply.github.com>
2022-11-20 14:04:16 +0530

    --

---
+++ src/main/java/com/example/nanitesting/NanitestingApplication.java
@@ -0,0 +1,14 @@
+package com.example.nanitesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanitestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanitestingApplication.class, args);
+		System.out.println("hui");
+	}
+
+}
