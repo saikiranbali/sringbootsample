a6a3cfa0d765a1537b1b8e1aac7a1a68a881a949
Sai Kiran <109275107+Saikirangg@users.noreply.github.com>
2022-11-20 16:40:29 +0530

    --

---
+++ src/main/java/com/example/nanitesting/Laptop.java
@@ -0,0 +1,39 @@
+package com.example.nanitesting;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+    private int lid;
+    private String brand;
+
+    public int getLid() {
+        return lid;
+    }
+
+    public void setLid(int lid) {
+        this.lid = lid;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop{" +
+                "lid=" + lid +
+                ", brand='" + brand + '\'' +
+                '}';
+    }
+
+    public void compile()
+    {
+        System.out.println("compiling");
+    }
+}
--- src/main/java/com/example/nanitesting/NanitestingApplication.java
+++ src/main/java/com/example/nanitesting/NanitestingApplication.java
@@ -2,13 +2,20 @@ package com.example.nanitesting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class NanitestingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NanitestingApplication.class, args);
-		System.out.println("hui");
+		ConfigurableApplicationContext context =
+				SpringApplication.run(NanitestingApplication.class, args);
+//		Alien a =context.getBean(Alien.class);
+		Alien b = context.getBean(Alien.class);
+//		a.setAid(3);
+//		System.out.println(a.getAid());
+//		a.show();
+		b.show();
 	}
 
 }
